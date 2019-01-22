0950dabafc2caa0b42e6e975845da44de806b09b
vokic <30930109+vokic@users.noreply.github.com>
2019-01-22 19:54:59 +0100

    Add files via upload

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,27 @@ import Utisak from './Utisak'
 import korisnici from '../data/korisnici.json'
 
 export default class Main extends Component {
+
+    state = {
+        randomKorisnici: []
+    }
+
+    componentDidMount() {
+        fetch('https://randomuser.me/api/?results=10')
+            .then(x => x.json())
+            .then(x => this.setState({
+                randomKorisnici: x.results
+            }))
+    }
+
     render() {
+        console.log(this.state.randomKorisnici)
+
+        // Mapirati randomKorisnici za HTML I prikazati ih
 
         const korisniciJsx = korisnici
-            .map((x, i) =>  <Utisak  key={i} korisnik={x} />
-            
+            .map((x, i) => <Utisak key={i} korisnik={x} />
+
             )
 
         return (
